ed5d9b3f229549fe0f7d83c555fbb8835e8ba199
aisuluusopubekova88@gmail.com <aisuluusopubekova88@gmail.com>
2023-03-15 12:38:29 -0500

    initial commit

---
+++ src/test/java/tests/Phone.java
@@ -0,0 +1,17 @@
+package tests;
+
+import utilities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+
+
+
+
+    }
+
+
+
+}
